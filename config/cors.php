486ac4a2c811bd7d8e40cd56cb072d8b6da36cec
mhairi19 <166132125+mhairi19@users.noreply.github.com>
2026-04-26 23:29:02 +0800

    open cors for testing

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 ﻿<?php
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
