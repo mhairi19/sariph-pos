0fda61ae4e574e257d8d553075c8df2908a21a15
mhairi19 <166132125+mhairi19@users.noreply.github.com>
2026-04-26 23:30:36 +0800

    add cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,9 @@
-<?php
+﻿<?php
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -10,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(function ($request, $next) {
+            $response = $next($request);
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            return $response;
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
